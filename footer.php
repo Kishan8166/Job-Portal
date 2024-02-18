05d0daabeeb7b72dfacb56aa1c8be5b6cf3ae378
Kishan Kumar <Kishankumar8166@gmail.com>
2024-02-19 00:57:07 +0530

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,37 @@
+
+
+<footer id="contactus" style="background-color: #272727;">
+    <div class="container" style="width: 100%;">
+				<div class="row justify-content-center">
+					<div class="col text-center">
+                        <img src="img/ftrlg.svg" style="width:60px;" alt="">
+                        <h2 class="footer-heading"> 
+                        <a href="index.php" style="color: #ffffff; font-family: 'Sora', sans-serif; font-size: 50px; text-decoration: none;"> Job Portal </a> 
+                        </h2>
+                    </div>	
+                                                
+                    <div class="menu text-center">
+                    <p>
+                    <a href="index.php" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> HOME </a> 
+                    <a href="#jbs" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> JOBS </a> 
+                    <a href="mailto: mahisingh8166@gmail.com" style="color: #ffd6a5; padding-top: 50px; font-family:Comfortaa;"> CONTACT </a>
+                    </p>
+                    </div>
+					
+				</div>
+				
+                <div class="row mt-5">
+					<div class="col-md-12 text-center">
+						<p class="copyrigt">
+                        <a style="color: #9FA4A9; text-decoration: none;"> All rights reserved 2022 | Made with ❤️️ by</a> 
+                        <a href="https://www.linkedin.com/in/bharti-kumari-3a991b257/" style="color: #FDFFAC;">Kishan</a></p>
+                        
+
+                        <!-- Instagram -->
+                        <a style="color: #ac2bac;" href="https://www.instagram.com/mahi.kishu/" role="button"
+                        ><i class="fab fa-instagram fa-lg"></i></a>
+
+					</div>
+				</div>
+    </div>
+</footer>
